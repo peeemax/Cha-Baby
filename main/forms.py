b763022728d9ebeebb3f9a39495ac80423c47901
peeemax <peeemax@hotmail.com>
2022-08-24 22:11:48 -0300

    Criado primeira view e o formulário da página

---
+++ main/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+
+class PessoaRecadoForm(forms.Form):
+    nome_familia = forms.CharField(required=True, label="Digite o seu nome ou da sua familia", max_length=256)
+    recado_familia = forms.CharField(required=True, label="Deixe o reacado para nossa família!", max_length=500)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class PessoaRecado(models.Model):
+    nome_familia = models.CharField(max_length=256)
+    recado = models.CharField(max_length=500)
+     
+ 
