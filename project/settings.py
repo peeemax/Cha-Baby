39bd8c0427a398bbec96a0397e199dc6c028dfed
peeemax <peeemax@hotmail.com>
2022-12-28 14:11:05 -0300

    Alterada fundo do index

--- project/settings.py
+++ project/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/staticfiles/'
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = '/staticfiles/'
 STATICFILE_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
